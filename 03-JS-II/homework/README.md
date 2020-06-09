03cf07ca02ea0997a37dd48180a0bd0c62d88a2c
xilen0x <carlos.astorgax@gmail.com>
2020-06-09 16:24:04 -0400

    terminados ejercicios 02-JS

--- 03-JS-II/homework/README.md
+++ 03-JS-II/homework/README.md
@@ -8,3 +8,14 @@
 * `&&`, `||`, `!`
 
 2. Desde la carpeta `Prep` en la carpeta donde clonaste el repo, ingresa el comando `npm test JSII.test.js` para correr los tests automatizados. Al principio, todos los tests estarán fallados/rotos. Encontrarás las funciones para hacer pasar los tests en el archivo `homework.js`.
+
+--------------------
+
+for : ciclo condicional, el cual se inicializa con una variable, luego una condicion, lo que permitirá salir del ciclo y por último el incremento de la variable creada.
+ej:    for(i=0; i<10; i++){}
+
+&&: Operador AND, que sirve cuando quiero que se cumplan 2 condiciones en la misma expresión.
+
+||: Operador OR, que sirve cuando quiero que se cumpla a lo menos una condicion.
+
+!: Operador NOT, que sirve cuando quiero que se cumpla la condición opuesta de la expresión.
--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,27 +5,48 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  if (x>y) {
+    return x;
+  } else if (x<y){
+    return y;
+  } else
+    return x;
 }
 
 function saludo(idioma) {
   // Devuelve un saludo en tres diferentes lenguajes:
-  // Si "idioma" es "aleman", devuelve "Guten Tag!"
-  // Si "idioma" es "mandarin", devuelve "Ni Hao!"
-  // Si "idioma" es "ingles", devuelve "Hello!"
-  // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
+  // Si "idioma" es "aleman", devuelve "Guten Tag!"  // Si "idioma" es "mandarin", devuelve "Ni Hao!"  // Si "idioma" es "ingles", devuelve "Hello!"  // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if (idioma == "aleman"){
+    return "Guten Tag!";
+  } else if(idioma == "mandarin"){
+    return "Ni Hao!";
+  } else if(idioma == "ingles"){
+    return "Hello!";
+  } else {
+    return "Hola!";
+  }
 }
-
 function esDiezOCinco(numero) {
     // Devuelve "true" si "numero" es 10 o 5
     // De lo contrario, devuelve "false"
     // Tu código:
+    if (numero === 10 || numero === 5) {
+      return true;
+    } else {
+      return false;
+    }
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero < 50 && numero > 20) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function esEntero(numero) {
@@ -36,6 +57,12 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  var entero = Math.floor(numero);
+  if (numero - entero === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function fizzBuzz(numero) {
@@ -43,7 +70,17 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+  if(numero%3==0 && numero%5==0){
+    return "fizzbuzz";
+  }else if (numero%3==0){
+    return "fizz";
+  }else if(numero%5==0){
+    return "buzz";
+  }else {
+    return numero;
+  }
 }
+
 
 function esPrimo(numero) {
   // Devuelve "true" si "numero" es primo
@@ -51,8 +88,16 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  if (numero !=0 && numero !=1) {
+    if (numero%2==0) {
+      return !true;
+    } else {
+      return !false;
+    }
+  }else {
+    return false;
+    }
 }
-
 
 
 // No modificar nada debajo de esta línea
